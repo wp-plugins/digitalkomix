9e2a3a320116b669662313c4e3247c4012fd78a2
andywar65 <andywar65@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-10 10:42:31 +0000

    commit package version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/digitalkomix/trunk@1005017 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ digitalkomix.php
@@ -0,0 +1,176 @@
+<?php 
+/*
+Plugin Name: digitalkOmiX
+Plugin URI: http://www.andywar.net/wordpress-plugins/digitalkomix-plugin
+Description: Creates a shortcode that displays balloons with text on an image.
+Version: 1.0
+Author: Andy War
+Author URI: http://www.andywar.net
+License: GPLv2
+*/
+
+/*  Copyright 2014  Andy War  (email : me@andywar.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action('wp_head', 'style_in_head');//add stiles in head of page
+
+function style_in_head(){//styles for comic frame
+	$css_url = plugins_url('digitalkomix/css/style.css' , '__FILE__');
+	$output="<link rel='stylesheet' id='digitalkomix-plugin-css'  href='".$css_url."' type='text/css' media='all' />";
+	echo $output;
+}
+
+add_shortcode('no-digkom', 'prevent_digitalkomix_shortcode');//shortcode to prevent shortcode from working
+
+function prevent_digitalkomix_shortcode($atts, $content=null){
+	return $content;
+}
+
+add_shortcode('digkom', 'digitalkomix_shortcode');//shortcode for comic frame
+
+function digitalkomix_shortcode($atts, $content=null){
+	$placeholder_url = plugins_url('digitalkomix/images/placeholder.jpg' , '__FILE__');
+	extract(shortcode_atts(array(//default settings for the comic frame
+		'image_link'=>'',
+		'image_url'=>$placeholder_url,
+		'width'=>'400',
+		'height'=>'600',
+		'caption'=>'',
+		'rows'=>'3',
+		'cols'=>'2',
+		'text_1'=>'',
+		'text_2'=>'',
+		'text_3'=>'',
+		'text_4'=>'',
+		'text_5'=>'',
+		'text_6'=>'',
+		'text_7'=>'',
+		'text_8'=>'',
+		'text_9'=>'',
+		'text_10'=>'',
+		'text_11'=>'',
+		'text_12'=>''
+	), $atts));
+	
+	if ($content !== ''){//if image has been inserted, we have to strip link, url, w, h
+		$content=strstr($content, 'href="');
+		$content=str_replace('href="', '', $content);
+		$image_link=strstr($content, '"', true);//image link
+		$content=strstr($content, 'src="');
+		$content=str_replace('src="', '', $content);
+		$image_url=strstr($content, '"', true);//image url
+		$content=strstr($content, 'width="');
+		$content=str_replace('width="', '', $content);
+		$width=strstr($content, '"', true);//image width
+		$content=strstr($content, 'height="');
+		$content=str_replace('height="', '', $content);
+		$height=strstr($content, '"', true);//image height
+	}
+	
+	if ($image_url == ''){//just in case rows, cols, link, url, w and h have been set to null
+		$image_url=$placeholder_url;
+	}
+	if ($image_link == ''){
+		$image_link= $image_url;
+	}
+	if ($rows == ''){
+		$rows='3';
+	}
+	if ($cols == ''){
+		$cols='2';
+	}
+	if ($width == ''){
+		$width='400';
+	}
+	if ($height == ''){
+		$height='600';
+	}
+	$caption=str_replace('&lt;', '<', $caption);//if sanitized
+	$caption=str_replace('&gt;', '>', $caption);
+	if (strpos($caption , '<bottom>')){//style for caption side
+		$caption=strstr($caption, '<bottom>', true);
+		$cap_b=' style="caption-side: bottom">';
+	} else {$cap_b='>';
+	}
+	
+	if ($rows * $cols > 12){//Control if cells are more than 12
+		$caption = 'WARNING! Rows x Cols > 12!';
+		$rows='4';
+		$cols='3';
+	}
+	
+	$vert = intval (100 / $rows);//cell width and height (percent value)
+	$hor = intval (100 / $cols);
+	
+	$text = array($text_1, $text_2, $text_3, $text_4, $text_5, $text_6, $text_7, $text_8, $text_9, $text_10, $text_11, $text_12);//populate text string
+	
+	$i = 0;
+	for ($r = 0 ; $r < $rows ; $r++){//populate text and percent array
+		for ($c = 0 ; $c < $cols ; $c++){
+			$text_array [$r] [$c] = $text [$i];
+			$percent_array [$r] [$c] = 'style="width: '.$hor.'%; height: '.$vert.'%;"';
+			$i++;
+		}
+	}
+	
+	$table = '';//initialize table
+	
+	for ($r = 0 ; $r < $rows ; $r++){//populate table
+		$table = $table.'<tr>';
+		for ($c = 0 ; $c < $cols ; $c++){
+			$text_array [$r] [$c] = str_replace('&lt;', '<',$text_array [$r] [$c]);//if sanitized
+			$text_array [$r] [$c] = str_replace('&gt;', '>',$text_array [$r] [$c]);
+			if (strpos($text_array [$r] [$c] , '<span ')){//check for cellspans
+				$span_arg=strstr($text_array [$r] [$c], '<span ');
+				$text_array [$r] [$c]=strstr($text_array [$r] [$c], '<span ', true);//deletes span argument from text
+				$span_arg=str_replace('<span ', '', $span_arg);
+				$rowspan=strstr($span_arg, ',', true);//strips rowspan
+				$span_arg=strstr($span_arg, ',');
+				$span_arg=str_replace(',', '', $span_arg);
+				$colspan=strstr($span_arg, '>', true);//strips colspan
+				$percent_array [$r] [$c] = 'style="width: '.$hor*$colspan.'%; height: '.$vert*$rowspan.'%;"';//corrects percent values for cell
+				$cellspan_array [$r] [$c] = '';
+				if ($rowspan > 1){
+					$cellspan_array [$r] [$c] = ' rowspan="'.$rowspan.'"';//set rowspan for cell
+				}
+				if ($colspan > 1){
+					$cellspan_array [$r] [$c] = $cellspan_array [$r] [$c].' colspan="'.$colspan.'"';//set colspan for cell
+				}
+				for ($rspan = 0 ; $rspan < $rowspan ; $rspan++){//kill text in spanned cells
+					for ($cspan = 0 ; $cspan < $colspan ; $cspan++){
+						if ($rspan+$cspan !== 0){//don't kill first cell!
+							$text_array [$r+$rspan] [$c+$cspan] = 'Skip Cell';
+						}
+					}
+				}
+			}
+			if ($text_array [$r] [$c] !== 'Skip Cell'){//if cellspan, deletes cell
+			$table = $table.'<td '.$cellspan_array [$r] [$c].' '.$percent_array [$r] [$c].'>'.$text_array [$r] [$c].'</td>';
+			}
+		}
+		$table = $table.'</tr>';
+	}
+	
+	//output of comic frame
+	return'<div class="digitalkomix_table">
+			<a href="'.$image_link.'" title="Click to view original image.">
+			<table style="width: '.$width.'px; height:'.$height.'px; background-image: url('.$image_url.')">
+			<caption'.$cap_b.$caption.'</caption>'
+			.$table.
+			'</table></a></div>';
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,73 @@
+=== digitalkOmiX ===
+Contributors: andywar65
+Donate link: http://www.andywar.net/wordpress-plugins/donate
+Tags: image, text, comics, balloons, shortcode
+Requires at least: 4.0
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin adds balloons to traditional comic frames. Balloons become part of the content of the post, and can be translated. 
+
+== Description ==
+
+This plugin adds balloons to traditional comic frames. Balloons become part of the content of the post, and can be translated. 
+Image becomes an HTML &lt;table&gt; background, while balloons are cells of that &lt;table&gt;.
+At this stage plugin works through a shortcode, that can be self-closing: [digkom]
+
+or enclosing: [digkom]&lt;IMAGE&gt;[/digkom]
+
+In the self-closing shortcode you will have to add image location and size, while the enclosing shortcode handles the image added
+directly from the Media Library. Below is a list of the shortcode attributes:
+
+Attributes for self-closing shortcode only
+
+*	image_url='URL of the image' : where your image is located.
+*	image_link='Link to your image' : you go there when you click on the image.
+*	width='Width of the image in pixels'
+*	height='Height of the image in pixels'
+
+General attributes
+
+*	rows='Number of rows' : how many rows of balloons you have on the image.
+*	cols='Number of columns' : how many columns of balloons you have on the image, please note that you may not have more than 
+	12 balloons on the image.
+*	caption='Text of the caption of the image' : text that will appear above the image, if you want it to appear on the bottom 
+	you have to add “&lt;bottom&gt;” at the end of the text.
+*	text_1='Text in the first balloon' : first balloon is in the top left corner of the image, last balloon is in the bottom right corner.
+*	text_nth='Text in the nth balloon' : position of the nth balloon depends on how many rows and columns you set.
+
+Please note that if you don’t write text in a balloon, the balloon will not be displayed. 
+If you want a balloon to span more than a cell, you have to add “&lt;span r,c&gt;" at the end of the text, 
+where “r” stands for rowspan and “c” stands for columnspan.
+
+== Installation ==
+
+1. Upload `digitalkomix` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Nothing else
+
+== Frequently Asked Questions ==
+
+= Does the plugin work on multisites? =
+
+Yes, it does.
+
+= Does it work on all themes? =
+
+It has been tested on twentythirteen and twentyfourteen.
+
+== Screenshots ==
+
+1. This is how the balloon appears on the frame.
+
+== Changelog ==
+
+= 1.0 =
+* First release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+No upgrades available.
